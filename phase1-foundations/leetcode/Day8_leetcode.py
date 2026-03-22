d3c1b43e3e2bfaab7c75b066d5b7ebc4f3ab2dda
Chandrasekhar Nuthalapati <nuthalapatichand186@gmail.com>
2026-03-22 22:40:27 +0530

    Update Day8_leetcode.py

--- phase1-foundations/leetcode/Day8_leetcode.py
+++ phase1-foundations/leetcode/Day8_leetcode.py
@@ -20,19 +20,20 @@ def hasCycle(head):
 # Approach: Iterative with dummy node
 # Time: O(m+n) | Space: O(1)
 
-def mergeTwoLists(l1, l2):
-    dummy = ListNode(0)    # dummy head
-    curr = dummy
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)    # dummy head
+        curr = dummy
 
-    while list1 and list2:
-        if list1.val <= list2.val:
-            curr.next = list1
-            list1 = list1.next
-        else:
-            curr.next = list2
-            list2 = list2.next
-        curr = curr.next
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
 
         # Attach remaining nodes
-    curr.next = list1 if list1 else list2
-    return dummy.next
+        curr.next = list1 if list1 else list2
+        return dummy.next
